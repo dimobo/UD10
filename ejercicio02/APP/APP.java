6c1f76b704e826e7fdd2859caef458113e62f4e1
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-08-06 13:41:14 +0200

    02 ok

--- ejercicio02/APP/APP.java
+++ ejercicio02/APP/APP.java
@@ -1,7 +1,17 @@
 package APP;
 
+import exepciones.MiExcepcion;
+
 public class APP {
 	
-	
+	public static void main(String[] args) {
+		try {
+			System.out.println("Mensaje mostrado por pantalla.");
+			throw new MiExcepcion(111);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Programa terminado.");
+	}
 
 }
